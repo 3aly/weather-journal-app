14249b5eee474fc66ebf0aa7bdbcc6465f5d0c4f
3aly <ali.hefny1998@gmail.com>
2022-08-22 17:33:37 +0200

    init

--- website/app.js
+++ website/app.js
@@ -3,3 +3,51 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+  const getData = async ( url = '')=>{
+    
+      const response = await fetch(url, {
+      method: 'GET', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      //body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+
+postData('/addMovie', {title: 'de7dsadsak'});
+postData('/addMovie', {title: 'de7sk'});
+getData('/');
